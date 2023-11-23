d825506bd764419915cb31554f3756805fa596bb
Emerson Rocha <52533818+Emerson-Rocha@users.noreply.github.com>
2023-11-23 08:05:04 -0300

    ASA

---
+++ CrudMAB/cadastrar.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+// referencia do mysql
+
+
+namespace CrubMab
+{
+    public partial class cadastrar : Form
+    {
+      
+        public cadastrar()
+        {
+            InitializeComponent();
+           
+
+        }
+
+        private void cadastro_Load(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void sair(object sender, EventArgs e)
+        {
+        }
+
+        private void BTCADASTRO_Click(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void CMBESTADO_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+ 
+       
+    }
+}
